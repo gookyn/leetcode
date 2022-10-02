e3be3f10344efb6f42b251869b98e07ea84d0862
Gookyn <liujinge.work@gmail.com>
2022-10-02 22:51:34 +0800

    feat: 赎金信

---
+++ 383.赎金信/s1.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function (ransomNote, magazine) {
+  // 目标字符大于源字符，肯定为 false
+  if (ransomNote.length > magazine.length) {
+    return false;
+  }
+
+  for (var i = 0; i < ransomNote.length; i++) {
+    // 只要有一个字符不存在，直接返回 false
+    if (magazine.indexOf(ransomNote[i]) === -1) {
+      return false;
+    }
+    // 如果字符存在，则删除一个该字符，继续遍历
+    magazine = magazine.replace(ransomNote[i], '');
+  }
+
+  return true;
+};
+
+/**
+ * str.indexof() 相当于一次循环，因此会增加时间复杂度
+ */
